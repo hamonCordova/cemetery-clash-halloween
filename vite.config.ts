670bd077a77770a7f415294175ec81ce623bef0c
hamon <hamon.cordova@airesdigital.com.br>
2024-10-16 10:29:12 -0300

    change dist directory

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
   },
   build: {
-    outDir: '../dist', // Output in the dist/ folder
+    outDir: 'dist', // Output in the dist/ folder
     emptyOutDir: true, // Empty the folder first
     sourcemap: true // Add sourcemap
   },
